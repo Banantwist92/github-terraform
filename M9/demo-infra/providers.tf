5e1d86517600d08a976363746908230096a9d0e4
Banantwist92 <157720910+Banantwist92@users.noreply.github.com>
2024-12-05 18:24:43 +0100

    Upload M9

---
+++ M9/demo-infra/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-adrian"
+    storage_account_name = "ssabackendadrbwa1l8idyl"
+    container_name       = "tfstate-adrian"
+    key                  = "demo-infram9.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
